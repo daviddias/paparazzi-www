93afe5a5f0d547d7be9e7c8faed954b57a365f65
David Dias <daviddias.p@gmail.com>
2015-04-25 20:02:25 +0100

    add a simple template

--- src/app/app.js
+++ src/app/app.js
@@ -6,22 +6,21 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/404');
 
     $stateProvider
-        .state('landing', {
+        .state('home', {
             url: '/',
-            templateUrl: 'partials/landing.html'
+            templateUrl: 'partials/home.html'
         })
-        .state('landing.contact-form', {
-            // url: '/list',
+       /*
+        .state('home.contact-form', {
+            url: '/list',
             templateUrl: 'partials/landing.contact-form.html',
             controller: function($scope) {
                 //$scope.items = ['A', 'List', 'Of', 'Items'];
             }
         })
+        */
         .state('404', {
             url: '/404',
-            templateUrl: 'partials/404.html',
-            controller: function($scope) {
-                //
-            }
+            templateUrl: 'partials/404.html'
         });
 });

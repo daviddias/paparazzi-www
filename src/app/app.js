3ad757af8fa2313679efb651888b29511a999fe2
David Dias <daviddias.p@gmail.com>
2015-04-27 23:32:09 +0100

    add form to register email

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 var angular = require('angular');
 
-var app = angular.module('paparrazi', [require('angular-ui-router')]);
+var app = angular.module('razzi', [require('angular-ui-router')]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/404');
@@ -8,17 +8,33 @@ app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('home', {
             url: '/',
+            abstract: true,
             templateUrl: 'partials/home.html'
         })
-       /*
-        .state('home.contact-form', {
-            url: '/list',
-            templateUrl: 'partials/landing.contact-form.html',
-            controller: function($scope) {
-                //$scope.items = ['A', 'List', 'Of', 'Items'];
+        .state('home.index', {
+            url: '',
+            templateUrl: 'partials/home.contact-form.html',
+            controller: function($scope, $state, $http) {
+                $scope.submit = function(email) {
+
+                    $http.post('http://localhost:9090/subscribe',
+                            {email:'random@email.com'})
+                        .success(function(data, status, headers, config) {
+                            $state.go('home.contact-form-submited');
+                        })
+                        .error(function(data, status, headers, config) {
+                            console.log('err: ' , status);
+                        });
+                };
             }
         })
-        */
+        .state('home.contact-form-submited', {
+            url: '',
+            templateUrl: 'partials/home.contact-form-submitted.html',
+            controller: function($scope) {
+
+            }
+        })
         .state('404', {
             url: '/404',
             templateUrl: 'partials/404.html'

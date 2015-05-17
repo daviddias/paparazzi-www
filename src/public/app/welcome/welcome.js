03e065da7b032bbfc6f5b90229347b842d094c1f
David Dias <daviddias.p@gmail.com>
2015-05-17 13:11:20 +0100

    bumbas

--- src/public/app/welcome/welcome.js
+++ src/public/app/welcome/welcome.js
@@ -13,6 +13,25 @@ angular.module('razzi.welcome', [
             })
         ;
     })
-    .controller('WelcomeCtrl', function($stateParams) {
-        console.log('SPEAKING FROM Welcome CONTROLLER')    
+    .controller('WelcomeCtrl', function($scope, $stateParams, $timeout) {
+      
+        var socket = io(http://localhost:3000);
+
+        $scope.waiting = false;
+        $scope.received = false;
+
+        $scope.submit = function(url) {
+            $scope.waiting = true;
+            console.log(url);  
+
+            //$timeout(function() {
+            //    $scope.waiting = false; 
+            //    $scope.received = true;
+            //},1000);
+
+            //1. get token
+            //2. register in websockets with the token
+            //3. submit the url 
+            //4. wait for it to get the image back
+        }
     });
